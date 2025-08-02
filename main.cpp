9b4f637384fed49a30e33ccf2d03e4c4bf73a3cb
nina <juan.amado.sicat.cleto@gmail.com>
2025-08-02 22:30:53 +0800

    Going to build sorter

--- main.cpp
+++ main.cpp
@@ -9,30 +9,80 @@ struct Node {
 };
 
 void Preorder(Node *node) {
+
     if (node != NULL) {
-        cout << node->value << endl;
+        cout << node->value << " ";
         Preorder(node->left);
         Preorder(node->right);
     }
+
 }
 
 void Inorder(Node *node) {
     if (node != NULL) {
         Inorder(node->left);
-        cout << node->value << endl;
+        cout << node->value << " ";
         Inorder(node->right);
     }
+
 }
 
 void Postorder(Node *node) {
+
     if (node != NULL) {
         Postorder(node->left);
         Postorder(node->right);
-        cout << node->value << endl;
+        cout << node->value << " ";
     }
+
 }
 
+void test() {
+    Node n1, n2, n3, n4, n5, n6, n7;
+
+    n1.value = 1;
+    n2.value = 2;
+    n3.value = 10;
+    n4.value = 20;
+    n5.value = 3;
+    n6.value = 42;
+    n7.value = 9;
+
+    Node root;
+    root.value = 5;
+    root.left = &n1;
+    root.right = &n3;
+
+    n1.left = NULL;
+    n1.right = &n2;
+
+    n2.left = NULL;
+    n2.right = &n5;
+
+    n3.left = &n7;
+    n3.right = &n4;
+
+    n7.left = NULL;
+    n7.right = NULL;
+
+    n4.right = &n6;
+    n4.left = NULL;
+
+    n6.right = NULL;
+    n6.left = NULL;
+
+    cout << "Preorder traversal: ";
+    Preorder(&root);
+    cout << endl;
+    cout << "Inorder traversal: ";
+    Inorder(&root);
+    cout << endl;
+    cout << "Postorder traversal: ";
+    Postorder(&root);
+    cout << endl;
+
+}
 
 int main() {
-
+    test();
 }
