24002db4f1fc1097d6d1de7cae1df6fb467e7f42
nina <juan.amado.sicat.cleto@gmail.com>
2025-08-02 22:14:29 +0800

    pre - clr in - lcr post - lrc

--- main.cpp
+++ main.cpp
@@ -8,6 +8,31 @@ struct Node {
     Node* right;
 };
 
+void Preorder(Node *node) {
+    if (node != NULL) {
+        cout << node->value << endl;
+        Preorder(node->left);
+        Preorder(node->right);
+    }
+}
+
+void Inorder(Node *node) {
+    if (node != NULL) {
+        Inorder(node->left);
+        cout << node->value << endl;
+        Inorder(node->right);
+    }
+}
+
+void Postorder(Node *node) {
+    if (node != NULL) {
+        Postorder(node->left);
+        Postorder(node->right);
+        cout << node->value << endl;
+    }
+}
+
+
 int main() {
 
 }
